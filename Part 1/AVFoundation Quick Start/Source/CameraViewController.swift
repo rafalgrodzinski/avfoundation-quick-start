f96e97aaa6eec23d4fde4e63872054011b3ecae4
Rafal Grodzinski <rafal@unalignedbyte.com>
2019-03-27 23:15:04 +0000

    Implemented camera preview

--- Part 1/AVFoundation Quick Start/Source/CameraViewController.swift
+++ Part 1/AVFoundation Quick Start/Source/CameraViewController.swift
@@ -7,7 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Setup session
+        let session = AVCaptureSession()
+        session.sessionPreset = .hd1920x1080
+        session.startRunning()
+
+        // Setup input
+        guard let inputDevice = AVCaptureDevice.default(for: .video) else {
+            return
+        }
+        guard let input = try? AVCaptureDeviceInput(device: inputDevice) else {
+            return
+        }
+        session.addInput(input)
+
+        // Setup preview
+        let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.videoGravity = .resizeAspectFill
+        previewLayer.frame = view.bounds
+        view.layer.addSublayer(previewLayer)
+    }
 }
 
