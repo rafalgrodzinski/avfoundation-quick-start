d6d9cd25b1d64b7589d1bb272873d8fb7029eff4
Rafal Grodzinski <rafal@unalignedbyte.com>
2019-03-27 17:48:07 +0000

    Set up the project

---
+++ Part 1/AVFoundation Quick Start/Source/CameraViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.swift
+//  AVFoundation Quick Start
+//
+//  Created by Rafal Grodzinski on 27/03/2019.
+//  Copyright © 2019 UnalignedByte. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+}
+
