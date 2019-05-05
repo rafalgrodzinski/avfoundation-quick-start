dd9ed65b7690e3fb07db3c654a5619dc7550e724
Rafal Grodzinski <rafal@unalignedbyte.com>
2019-05-05 16:56:56 +0100

    Renamed project

--- Part 2/AVFoundation Quick Start Part 2/Source/AppDelegate.swift
+++ Part 2/AVFoundation Quick Start Part 2/Source/AppDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.swift
-//  AVFoundation Quick Start
+//  AVFoundation Quick Start Part 2
 //
 //  Created by Rafal Grodzinski on 27/03/2019.
 //  Copyright © 2019 UnalignedByte. All rights reserved.
